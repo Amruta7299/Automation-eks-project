f6210a4163d11d480b3949f4276eba169942ad43
AK_07 <100669977+Amruta7299@users.noreply.github.com>
2026-04-16 20:15:38 +0530

    Removed helm and kubernetes provid
    Removed helm and kubernetes provider configurations.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,14 +5,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
+    
   }
 }
 
@@ -29,10 +22,4 @@ provider "aws" {
  # token                  = data.aws_eks_cluster_auth.cluster.token
 #}
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+
