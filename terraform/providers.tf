837e77c522b54bd1f96def28b45e4515e1fcd96d
AK_07 <100669977+Amruta7299@users.noreply.github.com>
2026-04-16 20:11:01 +0530

    chnge in provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,11 +23,11 @@ provider "aws" {
 # We dynamically configure the helm/kubernetes providers using the EKS module outputs
 
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+ # token                  = data.aws_eks_cluster_auth.cluster.token
+#}
 
 provider "helm" {
   kubernetes {
